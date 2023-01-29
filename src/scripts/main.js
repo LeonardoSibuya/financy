d0b0c9325b7eab3a70bb11f29c38b33bfa8f0ff6
e_sibuya@outlook.com <*e_sibuya@outlook.com*>
2023-01-28 21:45:18 -0300

    Criação das seções faq e form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]')
-
+    const questions = document.querySelectorAll('[data-faq-question]')
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -13,19 +13,8 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
-    function escondeAbas() {
-        const tabscontainer = document.querySelectorAll('[data-tab-id]')
-    
-        for (let i = 0; i < tabscontainer.length; i++) {
-            tabscontainer[i].classList.remove('market__acao--is-disable')
-        }
-    }
-
-    function removeBotaoAtivo() {
-        const buttons = document.querySelectorAll('[data-tab-button]')
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].classList.remove('market__buttons__verMais--is-active')
-        }
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta)
     }
 
     AOS.init();
@@ -40,3 +29,25 @@ document.addEventListener('DOMContentLoaded', function() {
         ibovVariation.innerHTML = json.public_repos;
     })
 })
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open'
+    const elementoPai = elemento.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+}
+
+function escondeAbas() {
+    const tabscontainer = document.querySelectorAll('[data-tab-id]')
+
+    for (let i = 0; i < tabscontainer.length; i++) {
+        tabscontainer[i].classList.remove('market__acao--is-disable')
+    }
+}
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]')
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('market__buttons__verMais--is-active')
+    }
+}
